2d5f5f16c4bd9c65dc659302a669c488d0294f2c
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-22 23:02:14 +0200

    hgkgjk

--- src/org/foo/ExcelCreator.groovy
+++ src/org/foo/ExcelCreator.groovy
@@ -4,8 +4,6 @@ import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
 
 class ExcelCreator extends PoiSpreadsheetBuilder{  
 
-    ExcelCreator(){}
-
     def gibText() {
         def text = "ich habe die Klasse gefunden"
         return text;
