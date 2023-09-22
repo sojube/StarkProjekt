1c6a1bd0359a8bd1fa492a26a14ae8a49f661c8a
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-22 20:30:01 +0200

    fjfjf

--- src/org/foo/ExcelCreator.groovy
+++ src/org/foo/ExcelCreator.groovy
@@ -1,5 +1,5 @@
 package org.foo
-@Grab(group='org.modelcatalogue', module='spreadsheet-builder-poi', version='0.4.1')
+@Grab(group='org.modelcatalogue', module='spreadsheet-builder-poi', version='')
 
 class ExcelCreator {  
 
