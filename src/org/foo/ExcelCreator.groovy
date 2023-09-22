10cd8fe44fea032ce9987b5596f138f3e1900d62
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-22 23:01:18 +0200

    gkjkjjk

--- src/org/foo/ExcelCreator.groovy
+++ src/org/foo/ExcelCreator.groovy
@@ -2,7 +2,7 @@ package org.foo
 @Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
 import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
 
-class ExcelCreator implements PoiSpreadsheetBuilder{  
+class ExcelCreator extends PoiSpreadsheetBuilder{  
 
     ExcelCreator(){}
 
