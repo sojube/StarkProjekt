760ae9ef448d519c14cd7ef5bb62d78ed8d580a3
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-22 23:26:03 +0200

    lökölö

--- src/org/foo/ExcelCreator.groovy
+++ src/org/foo/ExcelCreator.groovy
@@ -1,33 +1,24 @@
 package org.foo
 @Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
+
 import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
 
 File file = new File('spreadsheet.xlsx')
 
-class ExcelCreat {  
 
+def text = "ich habe die Klasse gefunden"
 
-    ExcelCreat(){}
-
-    def gibText() {
-        
-        def text = "ich habe die Klasse gefunden"
-
-        PoiSpreadsheetBuilder.create(file).build {                                              
-            sheet('Sample') {                                                                   
-                row {                                                                           
-                    cell 'A'                                                                    
-                    cell 'B'
-                    cell 'C'
-                }
-                row {
-                    cell 1
-                    cell 2
-                    cell 3
-                }
-            }
+PoiSpreadsheetBuilder.create(file).build {                                              
+    sheet('Sample') {                                                                   
+        row {                                                                           
+            cell 'A'                                                                    
+            cell 'B'
+            cell 'C'
         }
-        return text;
+        row {
+            cell 1
+            cell 2
+            cell 3
+        }
     }
-   
 }
