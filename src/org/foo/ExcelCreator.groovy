67665c734335b42449678198f6733f22e5533b53
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-23 00:27:06 +0200

    KJLKL

--- src/org/foo/ExcelCreator.groovy
+++ src/org/foo/ExcelCreator.groovy
@@ -5,22 +5,22 @@ import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
 
 File file = new File('tagesPlan.xlsx')
 
+def call(){
 
-PoiSpreadsheetBuilder.create(file).build {                                              
-    sheet('nameOfTheSheet') {                                                                   
-        row {                                                                           
-            cell 'A' { value 'Value 11' }                                                                 
-            cell 'B' { value 'Value 12' } 
-            cell 'C' { value 'Value 13' } 
-        }
-        row {
-            cell 1 { value 'Value 21' } 
-            cell 2 { value 'Value 22' } 
-            cell 3 { value 'Value 23' } 
+    PoiSpreadsheetBuilder.create(file).build {                                              
+        sheet('nameOfTheSheet') {                                                                   
+            row {                                                                           
+                cell 'A' { value 'Value 11' }                                                                 
+                cell 'B' { value 'Value 12' } 
+                cell 'C' { value 'Value 13' } 
+            }
+            row {
+                cell 1 { value 'Value 21' } 
+                cell 2 { value 'Value 22' } 
+                cell 3 { value 'Value 23' } 
+            }
         }
     }
-}
 
-def call(){
     return text = "j' étais ici"
 }
