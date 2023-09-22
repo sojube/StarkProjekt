b6978fb770719b3a7e16f9ab8263c964a55834ed
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-22 23:10:06 +0200

    fjfj

--- src/org/foo/ExcelCreator.groovy
+++ src/org/foo/ExcelCreator.groovy
@@ -2,7 +2,15 @@ package org.foo
 @Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
 import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
 
-class ExcelCreator extends PoiSpreadsheetBuilder{  
+def object = new PoiSpreadsheetBuilder();
+
+class ExcelCreator extends object {  
+
+    def data
+
+    ExcelCreator(String data){
+        this.data =
+    }
 
     def gibText() {
         def text = "ich habe die Klasse gefunden"
