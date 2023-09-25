102dfde5fb278ea129442fb26346f424e1a90ba9
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-25 10:56:40 +0200

    lklkllj

--- vars/buildExcel.groovy
+++ vars/buildExcel.groovy
@@ -1,7 +1,9 @@
 package org.foo
-//@Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
+@Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
 
-import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
+//import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
+
+def object = new PoiSpreadsheetBuilder();
 
 def call() {
 
