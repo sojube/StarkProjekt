adb7da029be5f32b567e5845365f3df86f8321e8
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-27 15:01:36 +0200

    jjhkkjhkk

--- vars/buildExcel.groovy
+++ vars/buildExcel.groovy
@@ -1,11 +1,14 @@
 package org.foo
 @Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
+//@Grab(group='builders.dsl', module='spreadsheet-builder-google', version='3.0.1')
+//@Grab(group='builders.dsl', module='spreadsheet-builder-groovy', version='3.0.1')
 
 import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
+import org.apache.poi.xssf.usermodel.XSSFSheet
 
 //def object = new PoiSpreadsheetBuilder();
 
-@NonCPS
+//@NonCPS
 def call() { 
 
       echo 'Ich war in der File buildExcel neue '
--- vars/varriable.groovy
+++ vars/varriable.groovy
@@ -1,4 +1,40 @@
+package org.foo
+@Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
 
-def call() {
-    echo 'Ich war in der File varriable'
+
+import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
+import junit.framework.Assert;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+
+
+def expo() {
+
+        echo "ich war in deer File variable"
+        // create a new file
+        //FileOutputStream file = new FileOutputStream("workbook.xls");
+        File file = new File('spreadsheet.xlsx')
+        // create a new workbook
+        SXSSFWorkbook wb = new SXSSFWorkbook();
+        // create a new sheet
+        Sheet sheet = wb.createSheet();
+        PoiSpreadsheetBuilder.create(file).build {                                              
+            sheet('Sample') {                                                                   
+                row {                                                                           
+                    cell 'A'                                                                    
+                    cell 'B'
+                    cell 'C'
+                }
+                row {
+                    cell 1
+                    cell 2
+                    cell 3
+                }
+            }
+        }
+       
 }
