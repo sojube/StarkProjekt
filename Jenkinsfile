6e36986c22af0c350b279bde8daf7723b09738e0
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-09-28 19:20:20 +0200

    kjhkkhk

--- Jenkinsfile
+++ Jenkinsfile
@@ -35,7 +35,9 @@ pipeline {
             steps{
 
                 script {
+
                     
+                    myCustomStep()
                     varriable()
                 }
                
--- vars/myCustomStep.groovy
+++ vars/myCustomStep.groovy
@@ -3,7 +3,7 @@
 def call() {
 
     // Add .jar files from the libs directory to the classpath
-    def libDir = "${JENKINS_HOME}/userContent/StarkProjekt/libs"
+    def libDir = "${JENKINS_HOME}/StarkProjekt/libs"
     def jars = findFiles(glob: "${libDir}/*.jar")
     jars.each { jar ->
         def jarPath = jar.toString()
