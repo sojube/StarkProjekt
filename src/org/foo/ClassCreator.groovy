51a94221383c564d6ee5d35373bf7585314101c8
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-04 12:52:25 +0200

    lklklkl

--- src/org/foo/ClassCreator.groovy
+++ src/org/foo/ClassCreator.groovy
@@ -1,5 +1,12 @@
 package org.foo
+@Grab(group='org.apache.poi', module='poi', version='5.0.0')
 @Grab(group='org.apache.poi', module='poi-ooxml', version='5.0.0')
+
+import java.io.FileOutputStream;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
