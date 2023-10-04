6577e114b71856a31a49fa55a275bf2faf3b8a0c
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-04 17:58:59 +0200

    zuzuzz

--- excelBuilder.groovy
+++ excelBuilder.groovy
@@ -43,7 +43,7 @@ workbook.write(fileOut)
 fileOut.close()
 
 //def newFile = new File(filePath)
-def newFile = new File("${WORKSPACE}/testJules6.txt")
+def newFile = new File("${WORKSPACE}/testJules7.txt")
 newFile.createNewFile() 
 FileOutputStream fileOute = new FileOutputStream(newFile);
 workbook.write(fileOute)
