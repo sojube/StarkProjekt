0eba73b9daf124ef033409884fc9d9b04bca5d50
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-04 14:49:00 +0200

    kkjkjkjkk

--- excelBuilder.groovy
+++ excelBuilder.groovy
@@ -11,41 +11,18 @@ Workbook workbook = new ClassCreator()
 // Erstelle ein neues Arbeitsblatt (Sheet)
 Sheet sheet = workbook.createSheet("Tabelle1")
 
-
-// Erstelle eine Kopfzeile
-
-Row headerRow = sheet.createRow(0)
-headerRow.createCell(0).setCellValue("Name")
-headerRow.createCell(1).setCellValue("Alter")
-headerRow.createCell(2).setCellValue("Beruf")
-
-// Füge Datenzeilen hinzu
-Row dataRow1 = sheet.createRow(1)
-dataRow1.createCell(0).setCellValue("Max Mustermann")
-dataRow1.createCell(1).setCellValue("dreißig")
-dataRow1.createCell(2).setCellValue("Ingenieur")
-
-Row dataRow2 = sheet.createRow(2)
-dataRow2.createCell(0).setCellValue("Erika Musterfrau")
-dataRow2.createCell(1).setCellValue("28")
-dataRow2.createCell(2).setCellValue("Lehrer") 
-
-
 // Specify the file path relative to the workspace
-//def filePath = "${WORKSPACE}/JulesBeispiel.xlsx"
-
 def filePath = "${WORKSPACE}/JulesBeispiel.xlsx"
 
 
-// Speichere die Arbeitsmappe in einer Datei
-//FileOutputStream fileOut = new FileOutputStream("filePath")
-FileOutputStream fileOut = new FileOutputStream(new File("filePath"));
-workbook.write(fileOut)
-fileOut.close()
+def workbook = new ExcelBuilder().build {
+    sheet {
+        row {
+            cell {
+                "test"
+            }
+        }
+    }
+}
 
-//def newFile = new File(filePath)
-def newFile = new File("${WORKSPACE}/testJules2.txt")
-newFile.createNewFile() 
-
-
-println("Die Excel-Datei wurde erfolgreich erstellt.")
+workbook.write(new FileOutputStream(new File(filePath)))
