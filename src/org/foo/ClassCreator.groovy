8209943aa50f7d0e287de63dccc94031ac69ea5e
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-04 15:09:49 +0200

    lklkllkj

--- src/org/foo/ClassCreator.groovy
+++ src/org/foo/ClassCreator.groovy
@@ -1,12 +1,14 @@
 package org.foo
 @Grab(group='org.apache.poi', module='poi', version='5.0.0')
 @Grab(group='org.apache.poi', module='poi-ooxml', version='5.0.0')
+@Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
 
 import java.io.FileOutputStream;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import builders.dsl.spreadsheet.builder.poi.PoiSpreadsheetBuilder
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
@@ -16,6 +18,18 @@ class ClassCreator extends XSSFWorkbook{
 
     XSSFWorkbook workbook = new XSSFWorkbook()
     return workbook;
+
+  }
+    
+
+}
+
+class SpreadsheetCreator extends PoiSpreadsheetBuilder{
+
+  def createrSpreadsheet(){
+
+    PoiSpreadsheetBuilder poiSpreadsheet = new PoiSpreadsheetBuilder()
+    return poiSpreadsheet;
 
   }
     
