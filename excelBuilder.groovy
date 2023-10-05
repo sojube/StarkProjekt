be3dd63f9b0230a1c9f6d54e55087c9b7ef20e68
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-05 10:18:41 +0200

    jkhkhh

--- excelBuilder.groovy
+++ excelBuilder.groovy
@@ -3,6 +3,8 @@
 import org.foo.ClassCreator
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.xssf.usermodel.XSSFRow
+import org.apache.poi.xssf.usermodel.XSSFSheet
+import java.io.FileOutputStream
 import org.apache.poi.ss.usermodel.CellBase
 import org.apache.poi.xssf.usermodel.XSSFCell
 import java.io.*;
@@ -15,7 +17,8 @@ Workbook workbook = new ClassCreator()
 Sheet sheet = workbook.createSheet("Tabelle1")
 
 // Erstelle eine Kopfzeile
-Row headerRow = sheet.createRow(0).getHeight(35)
+Row headerRow = sheet.createRow(0)
+headerRow.setHeightInPoints(35)
 headerRow.createCell(0).setCellValue("Kapitän")
 headerRow.createCell(1).setCellValue("Vorname Nachname")
 headerRow.createCell(2).setCellValue("Stand Up Datum")
@@ -26,6 +29,7 @@ headerRow.createCell(6).setCellValue("Sonstige")
 
 // Füge Datenzeilen hinzu
 Row dataRow1 = sheet.createRow(1)
+dataRow1.setHeightInPoints(35)
 dataRow1.createCell(0).setCellValue("Max Mustermann")
 dataRow1.createCell(1).setCellValue("dreißig")
 dataRow1.createCell(2).setCellValue("Ingenieur")
@@ -35,6 +39,7 @@ dataRow1.createCell(5).setCellValue("Ingenieur")
 dataRow1.createCell(6).setCellValue("Ingenieur")
 
 Row dataRow2 = sheet.createRow(2)
+dataRow2.setHeightInPoints(35)
 dataRow2.createCell(0).setCellValue("Erika Musterfrau")
 dataRow2.createCell(1).setCellValue("28")
 dataRow2.createCell(2).setCellValue("Lehrer")
@@ -42,6 +47,12 @@ dataRow2.createCell(3).setCellValue("Max Mustermann")
 dataRow2.createCell(4).setCellValue("dreißig")
 dataRow2.createCell(5).setCellValue("Ingenieur")
 dataRow2.createCell(6).setCellValue("Ingenieur") 
+
+// Einstellen der Zeilenhöhe
+//row.setHeightInPoints(30) // Hier kannst du die Höhe in Punkten anpassen
+
+// Einstellen der Spaltenbreite
+//sheet.setColumnWidth(0, 5000) // Hier kannst du die Breite in Einheiten anpassen
 
 
 // Specify the file path relative to the workspace
