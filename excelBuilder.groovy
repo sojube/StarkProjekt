a602eab0dc5ba5257c1de1311979e94cf7e9a3d7
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-05 12:14:24 +0200

    hjghjg

--- excelBuilder.groovy
+++ excelBuilder.groovy
@@ -1,7 +1,10 @@
 @Library('StarkProjekt') _ 
 
+@Grab(group='org.apache.poi', module='poi', version='5.0.0')
+@Grab(group='org.apache.poi', module='poi-ooxml', version='5.0.0')
+@Grab(group='builders.dsl', module='spreadsheet-builder-poi', version='3.0.1')
+
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import org.foo.ClassCreator
 import org.apache.poi.ss.usermodel.*
 import org.apache.poi.xssf.usermodel.XSSFRow
 import org.apache.poi.xssf.usermodel.XSSFSheet
