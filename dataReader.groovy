e511e52d4e65d5ab8d5a3b8a6f49614095d0a8c2
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-09 15:17:47 +0200

    zuztuztuzt

--- dataReader.groovy
+++ dataReader.groovy
@@ -12,9 +12,10 @@ String[] headerData = [];
 String[] headersHeadline = ["Data", "Vorname Nachname", "Zeit und Datum", "Intakevorbereitung", "Intake  mit:", "Zerziefizierung und Deadline", "Sonstige" ];
 String[] headersHeadline1 = ["Data1", "Vorname Nachname1", "Zeit und Datum1", "Intakevorbereitung1", "Intake  mit1:", "Zerziefizierung und Deadline1", "Sonstige1" ];
 String[] headersHeadline2 = ["Data2", "Vorname Nachname2", "Zeit und Datum2", "Intakevorbereitung2", "Intake  mit2:", "Zerziefizierung und Deadline2", "Sonstige2" ];
-data.add(headersHeadline);
-data.add(headersHeadline1);
-data.add(headersHeadline2);
+data.append(headersHeadline);
+data.append(headersHeadline1);
+data.append(headersHeadline2);
+
 
 println(data);
 
