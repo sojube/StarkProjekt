4015802385b5d8bb0c207b68b533f7a2d9d4b160
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-09 13:07:45 +0200

    zutzu

--- dataReader.groovy
+++ dataReader.groovy
@@ -1,12 +1,18 @@
 def inputFile = readFile(file: './tagesPlan.json')
 def jsonObject = readJSON text: inputFile
 
+// get all Headline i the json file and write it into a map
 def arrayOfKey = "${jsonObject[0].keySet()}"
 
 println "${arrayOfKey}"
 
 
 def listOfEmployees = [];
+
+for(item in jsonObject){
+    
+    println "${item}"
+}
 
 println " Die anzahl der Element für eine Person ist: ${jsonObject[0].size()}" 
 println " the size of json ist : ${jsonObject.size()}"  // die anzahl der Personnen in dem JSON
