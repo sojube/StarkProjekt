46beb16379f90efc45d04d635c0c507d24cd3a70
jules.sokoudjou <jules.sokoudjou@starkkraft-tech.de>
2023-10-12 13:04:16 +0200

    fgdrt

--- excelBuilderTest1.groovy
+++ excelBuilderTest1.groovy
@@ -18,13 +18,13 @@ import org.apache.poi.ooxml.*
 
 def workbook = new XSSFWorkbook();
 def sheet = workbook.createSheet("Blatt 1")
-
+FontCharset fontCharset = FontCharset.valueOf(charset);
 XSSFFont font = workbook.createFont();
 font.setItalic(true);
 //font.setCharSet(FontCharset.getCharset());
 
-println(FontCharset.getCharset())
-println(FontCharset.getCharset(0))
+println(fontCharset)
+
 
 CellStyle style = workbook.createCellStyle();
 
